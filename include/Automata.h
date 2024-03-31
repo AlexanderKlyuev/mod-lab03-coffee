eb6b2af421404201c994d75e753ef1acae5c6bfc
AlexanderKlyuev <161642862+AlexanderKlyuev@users.noreply.github.com>
2024-03-31 19:23:46 +0300

    Add files via upload

---
+++ include/Automata.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <map>
+
+class Automata {
+public:
+	enum STATES {
+		OFF,
+		WAIT,
+		ACCEPT,
+		CHECK,
+		COOK
+	};
+	void on();
+	void off();
+	void coin(int amount);
+	std::map<std::string, int> etMenu();
+	STATES getState();
+	int getCash();
+	std::string getSelectedDrink();
+	void choice(std::string drinkName);
+	void cancel();
+	void cook();
+	Automata();
+
+private:
+	STATES state;
+	int cash;
+	std::map<std::string, int> menu;
+	std::string selectedDrink;
+	void finish();
+};
